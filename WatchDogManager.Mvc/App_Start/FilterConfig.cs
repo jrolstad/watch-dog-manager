37eea77671de822c691b2f81d5439125abf2bf29
Josh Rolstad <jorolsta@microsoft.com>
2017-05-05 07:29:11 -0700

    Adding application insights

--- WatchDogManager.Mvc/App_Start/FilterConfig.cs
+++ WatchDogManager.Mvc/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace WatchDogManager.Mvc
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
