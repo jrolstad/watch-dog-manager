b855b8e9e79d397dc30f71c0e0b5518ce4047d5a
Josh Rolstad <jorolsta@microsoft.com>
2017-05-05 08:32:01 -0700

    Adding volunteer api controller

--- WatchDogManager.Mvc/Controllers/api/VolunteerController.cs
+++ WatchDogManager.Mvc/Controllers/api/VolunteerController.cs
@@ -3,39 +3,57 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
-using WatchDogManager.Mvc.Models.api;
+using WatchDogManager.EntityFramework;
+using WatchDogManager.Mvc.Application.Managers;
+using WatchDogManager.Mvc.Application.Mappers;
 
 namespace WatchDogManager.Mvc.Controllers.api
 {
     public class VolunteerController : ApiController
     {
+        private readonly VolunteerManager _manager;
+
+        public VolunteerController():this(new VolunteerManager(new WatchDogManagerDbContext(),new VolunteerMapper()))
+        {
+            
+        }
+        public VolunteerController(VolunteerManager manager)
+        {
+            _manager = manager;
+        }
+
         // GET: api/Volunteer
         public HttpResponseMessage Get()
         {
-            return Request.CreateResponse(new[] {new Volunteer()});
+            var result = _manager.Get();
+            return Request.CreateResponse(result);
         }
 
         // GET: api/Volunteer/5
         public HttpResponseMessage Get(int id)
         {
-            return Request.CreateResponse(new Volunteer());
+            var result = _manager.Get(id);
+            return Request.CreateResponse(result);
         }
 
         // POST: api/Volunteer
-        public HttpResponseMessage Post([FromBody]Volunteer value)
+        public HttpResponseMessage Post([FromBody]Models.api.Volunteer value)
         {
-            return Request.CreateResponse(new Volunteer());
+            var result = _manager.Create(value);
+            return Request.CreateResponse(result);
         }
 
         // PUT: api/Volunteer/5
-        public HttpResponseMessage Put(int id, [FromBody]Volunteer value)
+        public HttpResponseMessage Put(int id, [FromBody]Models.api.Volunteer value)
         {
-            return Request.CreateResponse(new Volunteer());
+            var result = _manager.Update(value);
+            return Request.CreateResponse(result);
         }
 
         // DELETE: api/Volunteer/5
         public HttpResponseMessage Delete(int id)
         {
+            _manager.Delete(id);
             return Request.CreateResponse(HttpStatusCode.OK);
         }
     }
