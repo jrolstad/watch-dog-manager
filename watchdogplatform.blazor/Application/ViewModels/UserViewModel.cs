315174db234bb4b2acf121c03697af2742433cd2
Josh Rolstad <jorolsta@microsoft.com>
2019-11-14 18:35:24 -0800

    Trying to add the user page

---
+++ watchdogplatform.blazor/Application/ViewModels/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace watchdogplatform.blazor.Application.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+
+        public void Load()
+        {
+            this.Name = Guid.NewGuid().ToString();
+        }
+    }
+
+    
+}
