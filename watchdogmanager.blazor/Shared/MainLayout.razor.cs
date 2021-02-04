70a8565975055b3dd6e17f5093cbd6a073a66703
Josh Rolstad <jorolsta@microsoft.com>
2021-02-04 13:37:53 -0800

    Adding general layout

---
+++ watchdogmanager.blazor/Shared/MainLayout.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace watchdogmanager.blazor.Shared
+{
+    public partial class MainLayout
+    {
+        protected void ModelDrawerHiddenChanged(bool hidden)
+        {
+            if (!hidden)
+            {
+                _navMenuOpened = false;
+            }
+        }
+
+
+        bool _navMenuOpened = false;
+
+        void MenuButtonClicked()
+        {
+            _navMenuOpened = !_navMenuOpened;
+            this.StateHasChanged();
+        }
+    }
+}
