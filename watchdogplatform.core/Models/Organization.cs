a277203edc3c3572a4618edda0bfae5adae7d62e
Josh Rolstad <jorolsta@microsoft.com>
2021-02-03 16:37:45 -0800

    Cleaning up old items for a re-try

--- watchdogplatform.core/Models/Organization.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace watchdogplatform.core.Models
-{
-    public class Organization
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string AffiliatedWith { get; set; }
-    }
-}
