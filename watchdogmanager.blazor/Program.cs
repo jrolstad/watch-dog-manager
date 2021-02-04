c235ef3164622bab0832d3e505fbb6bb50915a1e
Josh Rolstad <jorolsta@microsoft.com>
2021-02-04 12:22:05 -0800

    Using material design ui components

--- watchdogmanager.blazor/Program.cs
+++ watchdogmanager.blazor/Program.cs
@@ -17,12 +17,17 @@ namespace watchdogmanager.blazor
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.Logging.SetMinimumLevel(LogLevel.Warning);
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAuthorizationCore();
 
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+
+                options.ProviderOptions.LoginMode = "redirect";
             });
 
             builder.Services.AddMatBlazor();
