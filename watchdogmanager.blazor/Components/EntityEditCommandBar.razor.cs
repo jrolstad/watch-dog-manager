297b8324374f9151f22dbbe7414e21076b8c22f7
Josh Rolstad <jorolsta@microsoft.com>
2021-02-13 08:51:28 -0800

    Refactoring save cancel buttons to their own thing

---
+++ watchdogmanager.blazor/Components/EntityEditCommandBar.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace watchdogmanager.blazor.Components
+{
+    public partial class EntityEditCommandBar
+    {
+        [Parameter]
+        public Func<Task> OnSave { get; set; }
+        [Parameter]
+        public Func<Task> OnCancel { get; set; }
+    }
+}
