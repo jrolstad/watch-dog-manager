063750b4013b6a944240997dffe29c883e314822
Josh Rolstad <jorolsta@microsoft.com>
2021-02-13 08:44:37 -0800

    Refactoring headers so they can be shared

---
+++ watchdogmanager.blazor/Components/EntityHeader.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace watchdogmanager.blazor.Components
+{
+    public partial class EntityHeader
+    {
+        [Parameter]
+        public string Name { get; set; }
+
+        [Parameter]
+        public string Type { get; set; }
+    }
+}
---
+++ watchdogmanager.blazor/Components/EntityTypeHeader.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace watchdogmanager.blazor.Components
+{
+    public partial class EntityTypeHeader
+    {
+        [Parameter]
+        public string Type { get; set; }
+    }
+}
