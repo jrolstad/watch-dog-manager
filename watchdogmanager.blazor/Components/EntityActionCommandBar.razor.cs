5d3eacf0711425c09b7f5bafc1f90694b8b64e63
Josh Rolstad <jorolsta@microsoft.com>
2021-02-13 09:15:11 -0800

    Refactoring edit and delete buttons out

---
+++ watchdogmanager.blazor/Components/EntityActionCommandBar.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using watchdogmanager.blazor.Components.Organizations;
+
+namespace watchdogmanager.blazor.Components
+{
+    public partial class EntityActionCommandBar
+    {
+        [Parameter]
+        public string Id { get; set; }
+        [Parameter]
+        public GenericCommand<string> EditCommand { get; set; }
+        [Parameter]
+        public GenericCommand<string> DeleteCommand { get; set; }
+    }
+}
